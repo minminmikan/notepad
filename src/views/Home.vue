4f3f7680f6c425bcaba43a7816f46a32ee444143
remon2929 <ff.remon.9123@gmail.com>
2020-06-07 12:18:05 +0900

    編集画面の作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<ul v-for="memo in newest" :key="memo.id">
+  <li>{{ memo.body }}</li>
+</ul>
+
   </div>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+textarea{
+  width: 100%;
+  height:10em;
+}
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+button {
+  border: 1px solid #333 ;
+  background-color:#333;
+  color:#fff;
+  padding:10px 20px;
+  margin-top:10px;
+}
+</style>
+
+<script>
+export default{
+  name: "home",
+  computed:{
+    newest: function(){
+      return this.$store.state.memos.slice().reverse();
+    }
   }
 }
+
 </script>
